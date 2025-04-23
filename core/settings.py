1c7054b1247b3bb4e270ec07d9590ec137f8fd39
ErtAler <slavanamaev@gmail.com>
2025-04-23 18:05:27 +0300

    fafwad

--- core/settings.py
+++ core/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us-ru'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Asia/Bishkek'
 
